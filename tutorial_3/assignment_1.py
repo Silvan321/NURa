5417261a254caf447a36902d8337d02a8fa2a067
Silvan <silvan.toet@tno.nl>
2026-02-19 18:53:23 +0100

    added assignment 1 for tutorial 3. Tried to change Romberg to match lecture slides

---
+++ tutorial_3/assignment_1.py
@@ -0,0 +1,127 @@
+from math import pi
+
+import numpy as np
+
+
+def trapezoid(a, b, func, N: int):
+    """Use the extended trapezoid rule to calculate the integral.
+    Parameters a and b are start and stop x values of the range to be integrated respectively.
+    func is the function to be evaluated.
+    N is the number of evaluations.
+    """
+    xdata = np.linspace(a, b, num=N)
+    h = (b - a) / N  # step size
+    return h * (0.5 * (func(b) + func(a)) + np.sum(func(xdata[1:-1])))
+
+
+def simpson(a, b, func, N: int):
+    """Use the extended Simpson's rule to calculate the integral.
+    Parameters a and b are start and stop x values of the range to be integrated respectively.
+    func is the function to be evaluated.
+    N is the number of evaluations.
+    """
+    xdata = np.linspace(a, b, num=N)
+    h = (b - a) / N  # step size
+    _sum = 0
+    for index, value in enumerate(xdata):
+        if (index == 0) or (index == N - 1):
+            _sum += func(value)
+        elif index % 2 == 0:  # even terms
+            _sum += 4 * func(value)
+        else:  # odd terms except first and last
+            _sum += 2 * func(value)
+    return h / 3 * _sum
+
+
+def simpson_vectorized(a, b, func, N: int):
+    """Use the extended Simpson's rule to calculate the integral.
+    Vectorized version with array slizing for optimization -> time the difference!
+    Parameters a and b are start and stop x values of the range to be integrated respectively.
+    func is the function to be evaluated.
+    N is the number of evaluations.
+    """
+    xdata = np.linspace(a, b, num=N)
+    h = (b - a) / N  # step size
+    return h / 3 * (np.sum(4 * func(xdata[1::2])) + np.sum(2 * func(xdata[2:-1:2])) + func(xdata[0]) + func(xdata[-1]))
+
+
+def romberg(a, b, func, N_start: int, order: int):
+    """See slide 14 of Lecture 4 annotated slides for the formula used.
+    order is the number of initial approximations.
+    """
+    romberg_table = np.zeros((order + 1, order + 1))
+    for i in range(order):
+        N = (2**i) * N_start  # number of intervals doubles with each depth
+        romberg_table[i, 0] = trapezoid(a, b, func, N)
+        for j in range(1, i + 1):
+            i = i - 1
+            romberg_table[i, j] = (4**j * romberg_table[i + 1, j - 1] - romberg_table[i, j - 1]) / (4**j - 1)  # General version of 4/3 S_1 - 1/3 S_0
+    return romberg_table[0, order - 1]
+
+
+def x_squared(x):
+    return x * x
+
+
+def main():
+    # Analytical i.e. exact answers
+    x_squared_analytical = (5**3 - 1**3) / 3
+    print(f"{x_squared_analytical=}")
+    sin_x_analytical = 2
+    print(f"{sin_x_analytical=}")
+
+    # Numpy trapezoid
+    N = 8
+    print(f"{N=}")
+    x_squared_x_data = np.linspace(1, 5, num=N)
+    print(f"{np.trapz(x_squared_x_data**2, x_squared_x_data)=}")
+
+    sin_x_x_data = np.linspace(0, pi, num=N)
+    print(f"{np.trapz(np.sin(sin_x_x_data), sin_x_x_data)=}")
+    print()
+
+    # Assignment 1a
+
+    x_squared_trapz = trapezoid(1, 5, x_squared, N)
+    print(f"{x_squared_trapz=}")
+
+    sin_x_trapz = trapezoid(0, pi, np.sin, N)
+    print(f"{sin_x_trapz=}")
+
+    # Assignment 1b
+    x_squared_simp = simpson(1, 5, x_squared, 2 * N)
+    print(f"{x_squared_simp=}")
+    x_squared_simp_vec = simpson_vectorized(1, 5, x_squared, 2 * N)
+    print(f"{x_squared_simp_vec=}")
+
+    # Test vectorized vs non-vectorized version of Simpson's rule and time the difference
+    # repeat_statement_number = 10
+    # simpson_vectorized_time = timeit(lambda: simpson_vectorized(1, 5, x_squared, 2 * N), number=repeat_statement_number)
+    # print(f"{simpson_vectorized_time=} seconds")
+    # simpson_normal_time = timeit(lambda: simpson(1, 5, x_squared, 2 * N), number=repeat_statement_number)
+    # print(f"{simpson_normal_time=} seconds")
+
+    sin_x_simp = simpson(0, pi, np.sin, 2 * N)
+    print(f"{sin_x_simp=}")
+    sin_x_simp_vec = simpson_vectorized(0, pi, np.sin, 2 * N)
+    print(f"{sin_x_simp_vec=}")
+
+    # compare Simpsons method with 4/3 S_1 - 1/3 S_0. Because they have the same error term but the one from S_1 (2N steps)
+    # is a factor 4 smaller than the one from S_0 (N steps), we scale it with a factor 4.
+    x_squared_trapz_2N = trapezoid(1, 5, x_squared, 2 * N)
+    x_squared_trapz_combined = (4 / 3 * x_squared_trapz_2N) - (1 / 3 * x_squared_trapz)
+    print(f"{x_squared_trapz_combined=}")
+
+    sin_x_trapz_2N = trapezoid(0, pi, np.sin, 2 * N)
+    sin_x_trapz_combined = (4 / 3 * sin_x_trapz_2N) - (1 / 3 * sin_x_trapz)
+    print(f"{sin_x_trapz_combined=}")
+    # Apparently there is a small difference between Simpson's rule and the 4/3S_1 - 1/3S_0 rule which goes to 0 for N goes to infinity
+
+    # Romberg integration
+    x_squared_romberg = romberg(1, 5, x_squared, N_start=N, order=6)
+    print(f"Romberg result: {x_squared_romberg=}")
+    sin_x_romberg = romberg(0, pi, np.sin, N_start=N, order=6)
+    print(f"Romberg result: {sin_x_romberg=}")
+
+
+main()
