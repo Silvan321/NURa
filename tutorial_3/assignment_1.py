83ebf8d027ad55187e6235251c3fc5baab9c56c0
Silvan <silvan.toet@tno.nl>
2026-02-19 20:56:53 +0100

    Created a vector version for Romberg, all on my own! also did exercise 1d

--- tutorial_3/assignment_1.py
+++ tutorial_3/assignment_1.py
@@ -58,8 +58,27 @@ def romberg(a, b, func, N_start: int, order: int):
     return romberg_table[order - 1, order - 1]
 
 
+def romberg_vector_version(a, b, func, N_start: int, order: int):
+    """See slide 14 of Lecture 4 annotated slides for the formula used.
+    order is the number of initial approximations.
+    """
+    romberg_vector = np.zeros(shape=order)
+    for j in range(order):
+        for i in range(order - j):
+            if j == 0:  # the first time we fill the column, we use the trapezoid rule with doubling step sizes per row entry
+                N = (2**i) * N_start  # number of intervals doubles with each depth
+                romberg_vector[i] = trapezoid(a, b, func, N)
+            else:  # Subsequent times we use the update rule
+                romberg_vector[i] = (4**j * romberg_vector[i + 1] - romberg_vector[i]) / (4**j - 1)  # General version of 4/3 S_1 - 1/3 S_0
+    return romberg_vector[0]
+
+
 def x_squared(x):
     return x * x
+
+
+def q1d_func(x):
+    return 3 * np.exp(-2 * x) + (x / 10) ** 4
 
 
 def main():
@@ -116,11 +135,34 @@ def main():
     print(f"{sin_x_trapz_combined=}")
     # Apparently there is a small difference between Simpson's rule and the 4/3S_1 - 1/3S_0 rule which goes to 0 for N goes to infinity
 
-    # Romberg integration
+    # Assignment 1c: Romberg integration
     x_squared_romberg = romberg(1, 5, x_squared, N_start=N, order=6)
     print(f"Romberg result: {x_squared_romberg=}")
     sin_x_romberg = romberg(0, pi, np.sin, N_start=N, order=6)
     print(f"Romberg result: {sin_x_romberg=}")
+
+    x_squared_romberg_vector = romberg_vector_version(1, 5, x_squared, N_start=N, order=6)
+    print(f"Romberg_vector result: {x_squared_romberg_vector=}")
+    sin_x_romberg_vector = romberg_vector_version(0, pi, np.sin, N_start=N, order=6)
+    print(f"Romberg_vector result: {sin_x_romberg_vector=}")
+
+    # Assignment 1d
+    a, b = 0, 10
+    for n in [10, 100, 1000, 10000]:
+        q1d_func_trapz = trapezoid(a, b, q1d_func, n)
+        print(f"{n=}, {q1d_func_trapz=}")
+        q1d_func_simp_vec = simpson_vectorized(a, b, q1d_func, n)
+        print(f"{n=}, {q1d_func_simp_vec=}")
+
+    for m in [2, 4, 6]:
+        q1d_func_romberg = romberg(a, b, q1d_func, N_start=10, order=m)
+        print(f"{m=}, {q1d_func_romberg=}")
+        q1d_func_romberg_vec = romberg_vector_version(a, b, q1d_func, N_start=10, order=m)
+        print(f"{m=}, {q1d_func_romberg_vec=}")
+
+    # simpson is the most accurate, followed by trapezoid, after 10000 intervals
+    # But Romberg is already closer with m (order) = 6, at (so 2^m)=64 intervals, than trapz and simpson are at 1000
+    # Wolfram alpha calculation: integral_0^10 (3 exp(-2 x) + (x/10)^4) dx = 7/2 - 3/(2 e^20)≈3.5000
 
 
 main()
