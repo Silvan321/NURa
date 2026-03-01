57af661feebb7b484837edd43cefd97805cc9482
Toet <silvan.toet@tno.nl>
2026-03-01 22:07:31 +0100

    LU decomposition correctly implemented, 2a solved! :D

--- handins/Template_handinA1/Q2_solution.py
+++ handins/Template_handinA1/Q2_solution.py
@@ -2,9 +2,9 @@ import os
 import sys
 import timeit
 
-import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import numpy as np
 
 mpl.rcParams["font.size"] = 20
 mpl.rcParams["axes.labelsize"] = 20
@@ -13,11 +13,10 @@ mpl.rcParams["ytick.labelsize"] = 20
 
 
 def load_data():
-    """
-    Function to load the data from Vandermonde.txt.
+    """Function to load the data from Vandermonde.txt.
 
     Returns
-    ------------
+    -------
     x (np.ndarray): Array of x data points.
 
     y (np.ndarray): Array of y data points.
@@ -29,8 +28,7 @@ def load_data():
 
 
 def construct_vandermonde_matrix(x: np.ndarray) -> np.ndarray:
-    """
-    Construct the Vandermonde matrix V with V[i,j] = x[i]^j.
+    """Construct the Vandermonde matrix V with V[i,j] = x[i]^j.
 
     Parameters
     ----------
@@ -40,8 +38,6 @@ def construct_vandermonde_matrix(x: np.ndarray) -> np.ndarray:
     -------
     V : np.ndarray, Vandermonde matrix.
     """
-    # TODO:
-    # Replace with actual Vandermonde!
     V = np.zeros((len(x), len(x)), dtype=np.float64)
     for i, xvalue in enumerate(x):
         for j in range(len(x)):
@@ -49,91 +45,46 @@ def construct_vandermonde_matrix(x: np.ndarray) -> np.ndarray:
     return V
 
 
-def LU_decomposition(A: np.ndarray) -> np.ndarray:
-    """
-    Perform LU decomposition.
+class LUDecomposition:
+    def __init__(self, a: np.ndarray):
+        """This class performs LU decomposition, where a matrix A is decomposed in two matrices L and U.
+        Here L is a Lower Triangular matrix (only elements on or below diagonal), and U is an Upper triangular matrix (only elements on or above the diagonal).
+        We can use these to solve the equation A*x = (L*U)*x = L*(U*x) = b, by first solving L*y=b and then U*x=y.
+        Use the constructor of the class to do the decomposition, using Crout's algorithm (slide 12 lecture 3)
+        Then the instance of the object holds the LU decomposed matrix.
+        We can then call the solve method as many times as we like, for as many b's as we like.
+        """
+        self.nrows, self.ncolumns = a.shape[0], a.shape[1]
+        self.LU_matrix = np.identity(self.ncolumns)  # step 1: construct an identity matrix to start
+        for j in range(self.ncolumns):
+            self.LU_matrix[0, j] = a[0, j]  # for i = 0, beta_0j = a_0j
+            for i in range(1, self.nrows):  # starting from 0 or 1 should not make a difference, as the first row is already set to the correct values, but we can skip it to save some time
+                if i <= j:
+                    self.LU_matrix[i, j] = a[i, j] - sum(
+                        self.LU_matrix[i, k] * self.LU_matrix[k, j] for k in range(i)
+                    )  # beta_ij = a_ij - sum (alpha_ik beta_kj) from k=0 to i-1. range doesn't include end point!
+                if i > j:
+                    self.LU_matrix[i, j] = (a[i, j] - sum(self.LU_matrix[i, k] * self.LU_matrix[k, j] for k in range(j))) / self.LU_matrix[
+                        j, j
+                    ]  # alpha_ij = a_ij - sum (alpha_ik beta_kj) from k=0 to j-1. range doesn't include end point!
 
-    The lower-triangular matrix (L) is stored in the lower part of A (the diagonal elements are assumed =1),
-    while the upper-triangular matrix (U) is stored on and above the diagonal of A.
+    def get_LU_decomposition(self):
+        return self.LU_matrix
 
-    Parameters
-    ----------
-    A : np.ndarray
-        Matrix to decompose.
+    def solve(self, b: np.ndarray) -> np.ndarray:
+        # Forward substitution first: L*y=b
+        b_size = len(b)
+        y = np.zeros(b_size)
+        x = np.zeros(b_size)
+        y[0] = b[0]  # don't need to divide by LU_matrix[i,i] as for the forward substitution, the diagonal elements of L are all 1's
+        for i in range(1, b_size):
+            y[i] = b[i] - sum(self.LU_matrix[i, j] * y[j] for j in range(i))
 
-    Returns
-    -------
-    A : np.ndarray
-        Decomposed array.
-    """
-    # TODO:
-    # write your LU decomposition
-    return A
-
-
-def forward_substitution_unit_lower(LU: np.ndarray, b: np.ndarray) -> np.ndarray:
-    """
-    Solve L*y = b using forward substitution,
-    where L is the lower-triangular matrix.
-
-    Parameters
-    ----------
-    LU : np.ndarray
-        LU matrix from LU_decomposition.
-    b : np.ndarray
-        Right-hand side.
-
-    Returns
-    -------
-    y : np.ndarray
-        Solution vector.
-    """
-    # TODO:
-    # write your forward substitution for unit lower triangular matrix
-    return np.zeros_like(b)
-
-
-def backward_substitution_upper(LU: np.ndarray, y: np.ndarray) -> np.ndarray:
-    """
-    Solve U*c = y using backward substitution,
-    where U is the upper-triangular matrix.
-
-    Parameters
-    ----------
-    LU : np.ndarray
-        LU matrix from LU_decomposition.
-    y : np.ndarray
-        Right-hand side.
-
-    Returns
-    -------
-    c : np.ndarray
-        Solution vector.
-    """
-    # TODO:
-    # write your backward substitution for upper triangular matrix
-    return np.zeros_like(y)
-
-
-def vandermonde_solve_coefficients(x: np.ndarray, y: np.ndarray) -> np.ndarray:
-    """
-    Solve for polynomial coefficients c from data (x,y) using the Vandermonde matrix.
-
-    Parameters
-    ----------
-    x : np.ndarray
-        x-values.
-    y : np.ndarray
-        y-values.
-
-    Returns
-    -------
-    c : np.ndarray
-        Polynomial coefficients.
-    """
-    # TODO:
-    # solve V*c = y using your LU decomposition and forward/backward substitution
-    return np.zeros_like(x)  # Replace with your solution
+        # Now do the backsubstitution
+        x[b_size - 1] = y[b_size - 1] / self.LU_matrix[b_size - 1, b_size - 1]
+        for i in reversed(range(b_size)):
+            x[i] = (y[i] - sum(self.LU_matrix[i, j] * x[j] for j in range(i + 1, b_size))) / self.LU_matrix[i, i]
+        return x
 
 
 def evaluate_polynomial(c: np.ndarray, x_eval: np.ndarray) -> np.ndarray:
@@ -152,9 +103,15 @@ def evaluate_polynomial(c: np.ndarray, x_eval: np.ndarray) -> np.ndarray:
     y_eval : np.ndarray
         Polynomial values.
     """
-    # TODO:
     # evaluate the polynomial at x_eval using the coefficients c from vandermonde_solve_coefficients
-    return np.zeros_like(x_eval)  # Replace with your results
+    # evaluate efficiently using the formula on page 202 of the book "Numerical Recipes"
+    polynomial_at_evaluated_points = np.zeros_like(x_eval)
+    for i, x in enumerate(x_eval):
+        p = c[-1]
+        for j in reversed(range(len(c) - 1)):
+            p = p * x + c[j]
+        polynomial_at_evaluated_points[i] = p
+    return polynomial_at_evaluated_points
 
 
 def neville(x_data: np.ndarray, y_data: np.ndarray, x_interp: float) -> float:
@@ -379,12 +336,18 @@ def main():
     x_data, y_data = load_data()
     V = construct_vandermonde_matrix(x_data)
 
+    # I prefer a class implementation for the LU decomposition, based on the book "Numerical Recipes"
+    # The constructor does the decomposition, the getter allows access to LU matrix itself
+    ludcmp_instance = LUDecomposition(V)
+    V_lu = ludcmp_instance.get_LU_decomposition()
+
+    ludcmp_instance.solve(y_data)
     # compute times
     number = 10
 
     t_a = (
         timeit.timeit(
-            stmt=lambda: vandermonde_solve_coefficients(x_data, y_data),
+            stmt=lambda: ludcmp_instance.solve(y_data),
             number=number,
         )
         / number
@@ -412,8 +375,7 @@ def main():
         f.write(f"\\item Execution time for part (a): {t_a:.5f} seconds\n")
         f.write(f"\\item Execution time for part (b): {t_b:.5f} seconds\n")
         f.write(f"\\item Execution time for part (c): {t_c:.5f} seconds\n")
-
-    c_a = vandermonde_solve_coefficients(x_data, y_data)
+    c_a = ludcmp_instance.solve(y_data)
     plot_part_a(x_data, y_data, c_a)
 
     formatted_c = [f"{coef:.3e}" for coef in c_a]
