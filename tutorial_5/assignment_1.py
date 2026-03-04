8227c4f77f6e41e938b64d5c3cbb91a756660b97
Toet <silvan.toet@tno.nl>
2026-03-04 12:34:44 +0100

    WIP trying to implement iterative improvement in Ridders method

--- tutorial_5/assignment_1.py
+++ tutorial_5/assignment_1.py
@@ -1,4 +1,5 @@
 from collections.abc import Callable
+from copy import deepcopy
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -16,22 +17,39 @@ def central_difference(func: Callable, h: float, x: np.ndarray):
     return func(x + h) - func(x - h) / (2 * h)
 
 
-def ridder(func: Callable, h: float, x: np.ndarray, d: float = 2, m: int = 5):
+def ridder(func: Callable, h: float, x: np.ndarray, d: float = 2, m: int = 5, target_error: float = (np.finfo(float).eps) ** 0.8):
     """Use Ridder's method (Richardson extrapolation for differentiation) to compute the numerical derivative of a function func,
     using the central differences formula with an initial h.
     d is the factor to decrease h with for successive approximations.
     m is the number of initial approximations.
+    This approach is based on slide 8 of lecture 5, in such a way that it can be called with an x array at once.
+    We will continue improving our estimate until the best estimate is smaller than the target error.
+    The default target error is the machine relative error to the power 0.8, based on slide 9 of lecture 5.
     """
     D = np.zeros((m, len(x)))  # matrix storing m solution approximations for each x value in the input x array
-    D[0] = central_difference(func, h, x)
+
     d_inv = 1 / d  # calculate this once for efficiency
     h_vector = np.zeros(m)
     h_vector[0] = h  # for plotting purposes, remove when we want only efficiency.
-    for i in range(1, m):
-        h *= d_inv
-        h_vector[i] = h
-        D[i] = central_difference(func, h, x)
-    return D, h_vector
+    improvement = 2 * target_error  # make sure while loop starts
+    for j in range(m):
+        for i in range(m - j):
+            best_approx = deepcopy(D[0])
+            if j == 0 and i == 0:
+                D[0] = central_difference(func, h, x)
+            elif j == 0:
+                h *= d_inv
+                h_vector[i] = h
+                D[i] = central_difference(func, h, x)
+            else:
+                # Now combine pairs of approximations (step 3 of slide 8), but with update rule adapted for 1D version (overwriting of previous approximations)
+                D[i] = (d ** (2 * j) * D[i + 1] - D[i]) / (d ** (2 * j) - 1)
+            improvement = abs(best_approx - D[0])
+            if improvement < target_error:  # Terminate when improvement over previous best approx is smaller than target error
+                return D[0], h_vector
+            if improvement > previous_improvement:  # How can I measure if error grows? like this?
+                return best_approx
+            previous_improvement = improvement
 
 
 def main():
@@ -54,7 +72,6 @@ def main():
 
     # Assignment 1c
     # Use Ridder's method (Richardson extrapolation for differentiation)
-
     d = 2
     m = 5
     D, h_vector = ridder(func, h=0.1, x=x, d=d, m=m)
