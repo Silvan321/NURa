acde5ed62e0c0c44f452ca7d620078de38bc7b3e
Toet <silvan.toet@tno.nl>
2026-03-18 15:55:54 +0100

    added false position and newton raphson root finding methods. also added valueerror check (for now commented out) in secant and newton raphson when they diverge

--- tutorial_6/assignment_2.py
+++ tutorial_6/assignment_2.py
@@ -44,11 +44,63 @@ def secant(func: Callable, a: float, b: float, abs_err: float, rel_err: float, m
     iteration = 0
     while not np.isclose(fc, 0.0, rtol=rel_err, atol=abs_err) and iteration < max_number_of_iterations:
         c = b - ((b - a) / (fb - fa)) * fb
+        # if ((a - c) * (c - b)) < 0:  # a should be smaller than c and c should be smaller than b while inside bracket, so both negative, so product positive
+        #    raise ValueError("Error: Secant jumped out of bracket")
         fc = func(c)  # middle function evaluation
         iteration += 1
         b, a = c, b  # update b and a (x_i and x_i-1) to c and b (x_i+1 and x_i) respectively
         fa, fb = func(a), func(b)
     return c, iteration
+
+
+def false_position(func: Callable, a: float, b: float, abs_err: float, rel_err: float, max_number_of_iterations: int = 50):
+    """Implement the false position algorithm for finding a single root for a 1D function.
+    a and b are the boundaries of the interval in which the root should lie.
+    """
+    fa, fb = func(a), func(b)
+    if not fa * fb < 0:  # if the root is inside the bracket, one function evaluation is positive and the other is negative, so the product must be negative!
+        raise ValueError(
+            "Product of function evaluations not negative. If the root is inside the bracket, one function evaluation is positive and the other is negative, so the product must be negative!"
+        )
+
+    fc = -1
+    iteration = 0
+    while not np.isclose(fc, 0.0, rtol=rel_err, atol=abs_err) and iteration < max_number_of_iterations:
+        c = b - ((b - a) / (fb - fa)) * fb
+        fc = func(c)  # middle function evaluation
+        if fa * fc < 0:  # root between a and c
+            b = c
+            fb = func(b)
+        else:  # root between b and c
+            b, a = c, b
+            fa, fb = func(a), func(b)
+        iteration += 1
+    return c, iteration
+
+
+def newton_raphson_only(func: Callable, func_deriv: Callable, a: float, b: float, abs_err: float, rel_err: float, max_number_of_iterations: int = 50):
+    """Implement the Newton Raphson algorithm for finding a single root for a 1D function.
+    Contrary to the other root finding functions, this function also requires an analytical derivative of the function.
+    Calculating a numerical derivative is not worth it for efficiency: those calculations could be spend more efficiently on more iterations.
+    a and b are the boundaries of the interval in which the root should lie.
+    """
+    fa, fb = func(a), func(b)
+    if not fa * fb < 0:  # if the root is inside the bracket, one function evaluation is positive and the other is negative, so the product must be negative!
+        raise ValueError(
+            "Product of function evaluations not negative. If the root is inside the bracket, one function evaluation is positive and the other is negative, so the product must be negative!"
+        )
+
+    iteration = 0
+    c = (b - a) * 0.5  # initial guess
+    fd = -1
+    while not np.isclose(fd, 0.0, rtol=rel_err, atol=abs_err) and iteration < max_number_of_iterations:
+        d = c - func(c) / func_deriv(c)
+        # if ((a - d) * (d - b)) < 0:  # a should be smaller than d and d should be smaller than b while inside bracket, so both negative, so product positive
+        #     raise ValueError("Error: Newton-Raphson jumped out of bracket")
+        fd = func(d)
+        c = d
+        iteration += 1
+    return d, iteration
 
 
 def func2a(x):
@@ -71,12 +123,13 @@ def main():
 
     # Know thy function!
     func_list = [func2a, func2b, func2c, func2d]
-    algorithm_list = [bisection, secant]
+    algorithm_list = [bisection, secant, false_position]
     total_x = np.linspace(-2, 4, num=1000)
 
     fig, axs = plt.subplots(2, 2)
     plt.suptitle("Functions 2a-d over total x domain")
     for i, func in enumerate(func_list):
+        axs[i % 2, i // 2].plot(total_x, np.zeros(len(total_x)))
         axs[i % 2, i // 2].plot(total_x, func(total_x))
         axs[i % 2, i // 2].set_title(f"{func=}")
 
