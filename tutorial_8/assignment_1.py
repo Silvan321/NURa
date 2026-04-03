e423cb82b1f915dbe6087ee28290e8b6b72dcfe4
Toet <silvan.toet@tno.nl>
2026-04-03 19:06:47 +0200

    made Levenberg Marquardt iterative, but solution is diverging instead of converging

--- tutorial_8/assignment_1.py
+++ tutorial_8/assignment_1.py
@@ -27,42 +27,58 @@ def partial_c(x):
 
 
 class LevenbergMarquardt:
-    def __init__(self, x: np.ndarray, y: np.ndarray, partial_derivative_list: list[Callable], sigma):
-        self._construct_jacobian(x, partial_derivative_list)
-        self._construct_covariance_matrix(len(y), sigma)
-        self._calculate_alpha()
-        self._calculate_beta(y)
+    def __init__(self, x: np.ndarray, y: np.ndarray, partial_derivative_list: list[Callable], sigma, f: Callable, p: np.ndarray):
+        """Implement the Levenberg Marquardt algorithm.
+        x are the abscissa points of the measured data, y are the measured data values.
+        partial_derivative_list should contain a list of the partial derivatives of the function to be fitted, with the partial derivative to each parameter.
+        f is the function (model) to be fitted to the data, for which we try to find the best-fit parameters
+        p is the initial estimation of the parameter vector.
+        """
+        self.x = x
+        self.y_measured = y
+        self.sigma: Union[float, list, tuple, np.ndarray] = sigma  # noqa: UP007 use old typing for compatibility with vdesk
+        if np.any(self.sigma == 0):  # works for ints, floats, 1D and 2D arrays
+            raise ValueError("No value in sigma can be zero!")
+        self.f = f
+        self.p = p
+        self._construct_covariance_matrix()
 
-    def _construct_jacobian(self, x: np.ndarray, partial_derivative_list: list[Callable]):
+    def _calculate_model(self):
+        self.y_model = self.f(self.x, *self.p)  # unpack the vector of parameters into the function
+
+    def _construct_jacobian(self, partial_derivative_list: list[Callable]):
         """Construct the Jacobian, the matrix holding partial derivatives i.e. dau y_i/dau p_j for as set of datapoints and another set of parameters.
         So why do we supply an x array instead of a y array? because we fill in x in the analytical partial derivative of each parameter
         """
-        self.J = np.zeros((len(x), len(partial_derivative_list)))
-        for i, x_value in enumerate(x):
+        self.J = np.zeros((len(self.x), len(partial_derivative_list)))
+        for i, x_value in enumerate(self.x):
             for j, partial_derivative in enumerate(partial_derivative_list):
                 self.J[i, j] = partial_derivative(x_value)
 
-    def _construct_covariance_matrix(self, number_of_data_points: int, sigma: Union[float, list, tuple, np.ndarray]):  # noqa: UP007
+    def _construct_covariance_matrix(self):
+        number_of_data_points = len(self.y_measured)
         if isinstance(
-            sigma, (int, float)
+            self.sigma, (int, float)
         ):  # This is the case for Least Squares, where the standard deviation is constant for all x_i, and there is no correlation between the sigmas for various x_i's
-            cov = np.identity(number_of_data_points)
-            self.std2_inverse_matrix = np.identity(number_of_data_points) / sigma**2
+            self.std2_inv = 1 / self.sigma**2  # add to self for use in iterative calculation of chi square
+            self.std2_inverse_matrix = np.identity(number_of_data_points) * self.std2_inv
+
         elif isinstance(
-            sigma, (list, tuple)
+            self.sigma, (list, tuple)
         ):  # This is the case for Chi Squared, where the standard deviation can be different for each x_i, but there is no correlation between the sigmas for various x_i's
-            if len(sigma) != number_of_data_points:
+            if len(self.sigma) != number_of_data_points:
                 raise ValueError("Length of standard deviation iterable should match number of datapoints")
             cov = np.identity(number_of_data_points)
+            self.std2_inv_list = 1 / np.array(self.sigma) ** 2
             for i in range(len(cov)):
-                cov[i, i] /= sigma[i] ** 2
+                cov[i, i] *= self.std2_inv_list[i]
             self.std2_inverse_matrix = cov
         elif isinstance(
-            sigma, np.ndarray
+            self.sigma, np.ndarray
         ):  # This is the most general case where the standard deviation can be different for each x_i, AND there is can be correlation between the sigmas for various x_i's
-            if len(sigma) != number_of_data_points or sigma.shape[0] != sigma.shape[1]:
+            if len(self.sigma) != number_of_data_points or self.sigma.shape[0] != self.sigma.shape[1]:
                 raise ValueError("Length of standard deviation array should match number of datapoints and sigma should be a square array")
-            self.std2_inverse_matrix = 1 / sigma**2
+            self.std2_inverse_matrix = 1 / self.sigma**2
         else:
             raise TypeError("Unknown type supplied to construct covariance matrix method")
 
@@ -76,6 +92,7 @@ class LevenbergMarquardt:
         # Now for efficiency: we transpose the Jacobian which allows us to do matrix multiplication for each k,l combination
         # Row from k times i matrix times column i times l matrix
         self.alpha = self.J.T @ self.std2_inverse_matrix @ self.J
+        self.alpha_accent = self.alpha + np.eye(self.alpha.shape[0]) * self.lmbda
 
     def _calculate_beta(self, y: np.ndarray):
         # Do measured y value minus predicted model y value!
@@ -83,12 +100,48 @@ class LevenbergMarquardt:
             raise ValueError("y should be the same length as the number of rows of the Jacobian J!")
         self.beta = self.J.T @ self.std2_inverse_matrix @ y.T
 
-    def solve(self):
+    def _solve_delta_p(self):
         """Solve the linear system alpha delta_p = beta, for delta_p"""
-        return np.linalg.solve(self.alpha, self.beta)
+        return np.linalg.solve(self.alpha_accent, self.beta)
+
+    def _calculate_chisquare(self):
+        """We calculate the chi squared every time we make a new estimation of our parameter vector p.
+        The first will be a guess, the rest will follow from iteratively fitting our model to our data.
+        The fit results from solving the linear system alpha delta_p = b for delta_p, where delta_p is the change in the parameter vector
+        """
+        if not isinstance(self.sigma, (list, tuple)):
+            raise TypeError("sigma should be a list or tuple of standard deviations for use in chi square")
+        # calculate the expected y values by putting the x values together with the current parameter estimation into the function
+        return np.sum((self.y_measured - self.y_model) ** 2 * self.std2_inverse_matrix)
+
+    def iteratively_improve_solution(self, weight: float = 10.0, improvement_threshold: float = 0.01):
+        self._calculate_model()  # Calculate the y model values for the inicial parameter estimation p_0
+        self.old_chisquare = self._calculate_chisquare()  # Step 1: calculate chisquare for p_0
+        self.lmbda = 1e-3
+        self._construct_jacobian(partial_derivative_list)
+        self._calculate_alpha()
+        self._calculate_beta(self.y_measured)
+        delta_p = self._solve_delta_p()  # solve the linear system of equations to find the change in our parameter estimation
+        self.p += delta_p
+        self._calculate_model()
+        self.new_chisquare = self._calculate_chisquare()
+        while np.abs(self.new_chisquare - self.old_chisquare) > improvement_threshold:  # while solution keeps improving, keep going
+            if self.new_chisquare > self.old_chisquare:  # Old solution was better, keep old solution
+                self.lmbda *= weight  # We are far from the minimum, make bigger steps (more steepest descent)
+            else:  # New solution is better, update old solution to new solution
+                self.old_chisquare = self.new_chisquare
+                self.lmbda /= weight  # We are close to the minimum, make smaller steps (more Quasi Newton)
+            self._construct_jacobian(partial_derivative_list)
+            self._calculate_alpha()
+            self._calculate_beta(self.y_measured)
+            delta_p = self._solve_delta_p()  # solve the linear system of equations to find the change in our parameter estimation
+            self.p += delta_p
+            self._calculate_model()
+            self.new_chisquare = self._calculate_chisquare()
+        return self.p  # self.p now contains the parameters for the best fit!
 
 
-def main():
+if __name__ == "__main__":
     a = 2
     b = 1
     c = 2
@@ -111,8 +164,8 @@ def main():
     plt.plot(x, realizations[0])
     plt.show()
 
-    lm = LevenbergMarquardt(x, realizations[0], partial_derivative_list, sigma)
-    print(lm.solve())
+    sigma_list = [sigma for _ in range(number_of_datapoints)]
+    initial_p = np.array([2.1, 1.1, 2.1])
 
-
-main()
+    lm = LevenbergMarquardt(x, realizations[0], partial_derivative_list, sigma_list, func, initial_p)
+    print(lm.iteratively_improve_solution())
--- tutorial_9/assignment_1.py
+++ tutorial_9/assignment_1.py
@@ -1,28 +1,57 @@
 # Robust estimation
 
+import sys
 from pathlib import Path
 
-from matplotlib import pyplot as plt
+sys.path.append(str(Path(__file__).resolve().parents[1]))
 import numpy as np
+from matplotlib import pyplot as plt
+
+from tutorial_8.assignment_1 import LevenbergMarquardt
+
 # Only needed once
 # import wget
 
 # for i in range(1, 6):
 #     filename = wget.download(url=f"http://home.strw.leidenuniv.nl/~marinichenko/NUR_A/dataset{i}.txt")
 
-
-this_directory = Path(__file__).parent
-for file in this_directory.iterdir():
-    if file.is_file() and file.suffix == ".txt":
-        data = np.loadtxt(this_directory / file.name)
-        x = data[:, 0]
-        y = data[:, 1]
-        plt.figure()
-        plt.scatter(x, y, marker=".")
-        plt.show()
-
-
 # Fitting options:
 # 1. Levenberg Marquardt minimize chi squared
 # 2. Minimize model made with 2 params using e.g. downhill simplex
 # 3. builtin np.polyfit: gives coefficients
+
+
+def partial_a(x):
+    return x
+
+
+def partial_b(x):
+    return 1
+
+
+if __name__ == "__main__":
+    # 1a: Load and plot all datasets
+    dataset_to_study = 1
+    this_directory = Path(__file__).parent
+    for file in this_directory.iterdir():
+        if file.is_file() and file.suffix == ".txt":
+            data = np.loadtxt(this_directory / file.name)
+            x_loaded = data[:, 0]
+            y_loaded = data[:, 1]
+            # plt.figure()
+            # plt.scatter(x_loaded, y_loaded, marker=".")
+            # plt.show()
+
+    this_directory = Path(__file__).parent
+    data = np.loadtxt(this_directory / "dataset1.txt")  # Choose dataset 1 to start with
+    x = data[:, 0]
+    y = data[:, 1]
+
+    # 1b: linear fitting
+    # Let us use Levenberg Marquardt for Least Squares fitting by setting sigma constant for all x's
+    # A linear relation means we use the function f(x) = ax + b to fit the data
+    # Then our parameter partial derivatives are partial_a = x and partial b = 1
+    partial_derivative_list = [partial_a, partial_b]
+    sigma = 1  # Estimate not too big otherwise outliers are not outliers anymore
+    lm = LevenbergMarquardt(x, y, partial_derivative_list, sigma)
+    print(lm.solve_delta_p())
